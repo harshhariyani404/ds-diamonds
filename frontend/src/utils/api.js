42ab817fd24bdf9cc3305e4cc2fe751899c89b35
harshhariyani404 <harsh.hariyani404@gmail.com>
2026-01-09 17:08:57 +0530

    frontend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_URL = process.env.REACT_APP_API_URL || 'https://ds-diamonds.onrender.com';
+// const API_URL = process.env.REACT_APP_API_URL || 'https://ds-diamonds.onrender.com';
 
 // Create axios instance
 const api = axios.create({
